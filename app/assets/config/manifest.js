1597b977494c6f8fb9482b957d66b37b7ac7b635
ghazalshahzadi <ghazalshahzadi1@gmail.com>
2022-03-18 10:32:38 +0500

    real time chatapp

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,15 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-//= require rails-ujs
-//= require jquery
-//= require activestorage
-//= require turbolinks
-//= require semantic-ui
-//= require_tree .
 
-
-$(document).on('turbolinks:load', function() {
-    $('.ui.dropdown') .dropdown();
-})
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User1.create(username: "ghazal", password: "ghazal")
+User1.create(username: "Mudasra", password: "ghazal")
+User1.create(username: "Salwa", password: "ghazal")
+User1.create(username: "Suman", password: "ghazal")
+User1.create(username: "Zoe", password: "ghazal")
+
